3fb066fda263c1e643d0ef5a869d38ece06db0f1
hamzahassan54 <documentarytvofficial007@gmail.com>
2021-09-01 17:35:44 +0500

    made a connection with database ans instaall some packges

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { mongo } = require('mongoose');
+dotenv.config();
+const mongoose = require('./config/db');
+
+
+const app = express();
+
+
+var corsOptions = {
+    origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// you can also use this 
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+var port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`server started running on port :${port}`)
+})
